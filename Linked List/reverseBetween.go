2c52a7cbbe6f5332abd9390d369f9931112cc70d
Arnav Singh <arnav@wrath.com>
2023-05-30 13:45:38 +0530

    reverse linked list 2

---
+++ Linked List/reverseBetween.go
@@ -0,0 +1,36 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseBetween(head *ListNode, left, right int) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	var prevNode, tempNode, beforeLeftNode *ListNode
+	currNode := head
+	pos := 1
+	dummy := &ListNode{Val: 0, Next: head}
+	beforeLeftNode = dummy
+	for pos < left {
+		beforeLeftNode = currNode
+		currNode = currNode.Next
+		pos++
+	}
+
+	for pos <= right {
+		tempNode = currNode.Next
+		currNode.Next = prevNode
+		prevNode = currNode
+		currNode = tempNode
+		pos++
+	}
+
+	beforeLeftNode.Next.Next = currNode
+	beforeLeftNode.Next = prevNode
+
+	return dummy.Next
+}
