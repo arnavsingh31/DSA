0969006c8686daab7702a14c0c7111a5bc2b51e8
Arnav Singh <arnav@wrath.com>
2023-05-31 14:01:23 +0530

    reverse node in k group approach 2

--- Linked List/reverseNodesInGroup.go
+++ Linked List/reverseNodesInGroup.go
@@ -6,6 +6,8 @@ type ListNode struct {
 }
 
 // APPROACH #1 Recursion.
+// T.C---> O(n) as each node is visited only once.
+// S.C---> O(n/k) as in each recursive call we reverse k nodes so to reverse 'n' nodes call stack will use space of n/k.
 // We will reverse first k nodes rest recursion will handle.
 func reverseKGroup(head *ListNode, k int) *ListNode {
 
@@ -61,4 +63,52 @@ func helper(head *ListNode, k, diff int) *ListNode {
 
 	// return head of reversed list
 	return prevNode
+}
+
+// APPROACH #2
+// T.C---> O(n)
+// S.C---> O(1)
+func reverseKGroup2(head *ListNode, k int) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	dummyNode := &ListNode{Next: head}
+	nodeBeforeGroup := dummyNode
+	tempNode := &ListNode{}
+	prevNode := &ListNode{}
+
+	for true {
+		kthNode := geKthNode(nodeBeforeGroup, k)
+
+		if kthNode == nil {
+			break
+		}
+		nodeAfterGroup := kthNode.Next
+
+		// line 90 understand it via dry run
+		prevNode = kthNode.Next
+		currNode := nodeBeforeGroup.Next
+		for currNode != nodeAfterGroup {
+			tempNode = currNode.Next
+			currNode.Next = prevNode
+			prevNode = currNode
+			currNode = tempNode
+		}
+
+		// line 100- ***very important. do a dry run then you will understand.
+		tmp := nodeBeforeGroup.Next
+		nodeBeforeGroup.Next = kthNode
+		nodeBeforeGroup = tmp
+	}
+	return dummyNode.Next
+}
+
+func geKthNode(currNode *ListNode, k int) *ListNode {
+
+	for currNode != nil && k > 0 {
+		currNode = currNode.Next
+		k--
+	}
+	return currNode
 }
