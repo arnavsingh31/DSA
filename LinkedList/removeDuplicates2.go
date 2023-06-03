b171111d78ee23b7b1be449ef2c6458011ee35f9
Arnav <arnav.xddd@gmail.com>
2023-06-03 11:13:16 +0530

    remove all duplicates in the linked list

---
+++ LinkedList/removeDuplicates2.go
@@ -0,0 +1,33 @@
+package main
+
+func removeDuplicatesFromSortedList2(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	dummy := &ListNode{Val: -101, Next: head}
+	deleteNode := &ListNode{}
+	nextDeleteNode := &ListNode{}
+	prev := dummy
+
+	currNode := prev.Next
+
+	for currNode != nil {
+
+		if (currNode.Next != nil) && (currNode.Val == currNode.Next.Val) {
+			deleteNode = currNode
+			nextDeleteNode = currNode.Next
+		}
+
+		if currNode == deleteNode || currNode == nextDeleteNode {
+			prev.Next = currNode.Next
+			currNode = currNode.Next
+			continue
+		}
+
+		prev = currNode
+		currNode = currNode.Next
+	}
+
+	return dummy.Next
+}
