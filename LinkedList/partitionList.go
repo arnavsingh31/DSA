6402e4bcabf991ae1d7533d04d98f5a503d8062b
Arnav <arnav.xddd@gmail.com>
2023-06-03 23:47:50 +0530

    partion the given linked lits

---
+++ LinkedList/partitionList.go
@@ -0,0 +1,39 @@
+package main
+
+func partitionList(head *ListNode, x int) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	dummyNode := &ListNode{Next: head}
+	largeHead := &ListNode{}
+	largePtr := &ListNode{}
+	smallPtr := &ListNode{}
+	smallhead := &ListNode{}
+
+	currNode := dummyNode.Next
+
+	for currNode != nil {
+		if currNode.Val < x {
+			if smallhead.Next == nil {
+				smallhead.Next = currNode
+			}
+			smallPtr.Next = currNode
+			smallPtr = currNode
+		} else { // currNode.Val >= x
+			if largeHead.Next == nil {
+				largeHead.Next = currNode
+			}
+			largePtr.Next = currNode
+			largePtr = currNode
+		}
+		currNode = currNode.Next
+	}
+	if smallhead.Next == nil {
+		return largeHead.Next
+	}
+	smallPtr.Next = largeHead.Next
+	largePtr.Next = nil
+
+	return smallhead.Next
+}
