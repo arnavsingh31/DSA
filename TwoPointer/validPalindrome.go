e6a9e5ca6011676510ea96b326af9e70c8b23e97
Arnav <arnav.xddd@gmail.com>
2023-06-08 23:32:48 +0530

    given string is palindrome or not

---
+++ TwoPointer/validPalindrome.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func validPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	newStr := ""
+
+	for _, char := range s {
+		if isValid(byte(char)) {
+			newStr += string(char)
+		}
+	}
+
+	if newStr == "" {
+		return true
+	}
+
+	start := 0
+	end := len(newStr) - 1
+
+	for start <= end {
+		if newStr[start] != newStr[end] {
+			return false
+		}
+		start++
+		end--
+	}
+	return true
+
+}
+
+// this code is shorter version of the code above.
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	for left, right := 0, len(s)-1; left < right; left, right = left+1, right-1 {
+		for left < right && !isValid(s[left]) {
+			left++
+		}
+		for left < right && !isValid(s[right]) {
+			right--
+		}
+		if s[left] != s[right] {
+			return false
+		}
+	}
+	return true
+}
+
+func isValid(char byte) bool {
+	return (char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')
+}
+
+// using regexp
+func validPalindrome2(s string) bool {
+	s = strings.ToLower(s)
+	newStr := clearString(s)
+
+	if newStr == "" {
+		return true
+	}
+
+	start := 0
+	end := len(newStr) - 1
+
+	for start <= end {
+		if newStr[start] != newStr[end] {
+			return false
+		}
+		start++
+		end--
+	}
+	return true
+}
+
+func clearString(s string) string {
+	var nonAlphanumericRegex = regexp.MustCompile(`[^a-zA-Z0-9]+`)
+
+	return nonAlphanumericRegex.ReplaceAllString(s, "")
+
+}
