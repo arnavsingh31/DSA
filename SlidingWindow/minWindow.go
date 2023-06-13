ce93dcae68a9ac450e58f06ed06f7194b0024180
Arnav <arnav.xddd@gmail.com>
2023-06-13 19:22:09 +0530

    optimised

--- SlidingWindow/minWindow.go
+++ SlidingWindow/minWindow.go
@@ -1,7 +1,8 @@
 package main
 
-import "log"
-
+// T.C----->O(m+n) m = length of s and n = length of t. S.C ---->O(m+n).
+// please refere the link for better understanding:
+// https://leetcode.com/problems/minimum-window-substring/editorial/?envType=study-plan-v2&envId=top-interview-150
 func minWindow(s string, t string) string {
 
 	sLen := len(s)
@@ -20,38 +21,30 @@ func minWindow(s string, t string) string {
 	for i := 0; i < tLen; i++ {
 		mapT[t[i]] += 1
 	}
-	log.Printf("mapT is ---->%v", mapT)
 
 	for right := 0; right < sLen; right++ {
-		log.Printf("s.right is ----->%v", string(s[right]))
+
 		if val, exist := mapT[s[right]]; exist {
 			windowMap[s[right]] += 1
 			if windowMap[s[right]] == val {
 				charFound++
-				log.Printf("charFound=======>%v", charFound)
 			}
 		}
-		log.Printf("window map is ---->%v", windowMap)
 
 		for charFound == len(mapT) && left <= right {
-			log.Print("here1")
-			log.Printf("left----> %v, right---->%v", left, right)
+
 			if posArr[0] == -1 || (right-left+1) < posArr[0] {
-				log.Print("here2")
 				posArr[0] = right - left + 1
 				posArr[1] = left
 				posArr[2] = right
-				log.Printf("posArray-------> %v", posArr)
 			}
-			log.Printf("s.left---%v", string(s[left]))
+
 			if _, exist := windowMap[s[left]]; exist {
 				windowMap[s[left]] -= 1
-				log.Printf("window map is ---->%v", windowMap)
 			}
 
 			if count, exist := mapT[s[left]]; exist && windowMap[s[left]] < count {
 				charFound--
-				log.Printf("charFound2=======>%v", charFound)
 			}
 
 			left++
@@ -66,13 +59,71 @@ func minWindow(s string, t string) string {
 	return s[posArr[1] : posArr[2]+1]
 }
 
-func main() {
-	log.Printf("min window substring is :- %v", minWindow("aa", "aa"))
+// Approach 2
+type Pair struct {
+	index    int
+	charByte byte
 }
 
-/*
-dry run:
+func minWindow2(s, t string) string {
+	sLen := len(s)
+	tLen := len(t)
 
+	if sLen == 0 || tLen == 0 {
+		return ""
+	}
 
+	mapT := make(map[byte]int)
 
-*/
+	for i := 0; i < tLen; i++ {
+		mapT[t[i]] += 1
+	}
+
+	filteredS := []Pair{}
+
+	for i := 0; i < sLen; i++ {
+		if _, exist := mapT[s[i]]; exist {
+			indexCharPair := Pair{index: i, charByte: s[i]}
+			filteredS = append(filteredS, indexCharPair)
+		}
+	}
+
+	left := 0
+	charFound := 0
+	windowMap := make(map[byte]int)
+	posArr := []int{-1, 0, 0}
+
+	for right := 0; right < len(filteredS); right++ {
+		if count, exist := mapT[filteredS[right].charByte]; exist {
+			windowMap[filteredS[right].charByte] += 1
+
+			if windowMap[filteredS[right].charByte] == count {
+				charFound++
+			}
+		}
+
+		for charFound == len(mapT) && left <= right {
+			rightIndex := filteredS[right].index
+			leftIndex := filteredS[left].index
+			if posArr[0] == -1 || rightIndex-leftIndex+1 < posArr[0] {
+				posArr[0] = rightIndex - leftIndex + 1
+				posArr[1] = leftIndex
+				posArr[2] = rightIndex
+			}
+
+			if _, exist := windowMap[filteredS[left].charByte]; exist {
+				windowMap[filteredS[left].charByte] -= 1
+			}
+
+			if count, exist := mapT[filteredS[left].charByte]; exist && windowMap[filteredS[left].charByte] < count {
+				charFound--
+			}
+			left++
+		}
+	}
+
+	if posArr[0] == -1 {
+		return ""
+	}
+	return s[posArr[1] : posArr[2]+1]
+}
