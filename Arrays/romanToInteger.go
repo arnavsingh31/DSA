4917b44ede1aaacdc321e4188aef3139d9b150c2
Arnav <arnav.xddd@gmail.com>
2023-06-18 01:13:50 +0530

    convert roman to integer

---
+++ Arrays/romanToInteger.go
@@ -0,0 +1,26 @@
+package main
+
+func romanToInt(s string) int {
+	roman_To_Int_Map := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	integer := 0
+
+	for i := 0; i < len(s); i++ {
+		curr := roman_To_Int_Map[s[i]]
+		if i+1 < len(s) && curr < roman_To_Int_Map[s[i+1]] {
+			integer -= curr
+		} else {
+			integer += curr
+		}
+	}
+
+	return integer
+}
