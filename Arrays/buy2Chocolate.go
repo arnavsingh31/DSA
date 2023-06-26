c035474fba3937c4f29dfa9d8dde6a00228421d8
Arnav <arnav.xddd@gmail.com>
2023-06-27 02:43:54 +0530

    buy 2 chocolate with max saving

---
+++ Arrays/buy2Chocolate.go
@@ -0,0 +1,26 @@
+package main
+
+// approach using two pointers
+func buyChoco(prices []int, money int) int {
+	left, right := 0, len(prices)-1
+
+	savings := -1
+
+	for left < right {
+		cost := prices[left] + prices[right]
+		if money-cost > savings {
+			savings = money - cost
+		}
+
+		if prices[left] > prices[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	if savings < 0 {
+		return money
+	}
+	return savings
+}
