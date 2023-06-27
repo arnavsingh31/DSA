9f7862de65af349a83475bcf947d534d97bb5c69
Arnav <arnav.xddd@gmail.com>
2023-06-28 00:56:09 +0530

    efficient method for calc. power of a number

--- Random/pow.go
+++ Random/pow.go
@@ -38,3 +38,32 @@ func pow2(x, n int) int {
 	// 	res = pow2(x, n-1) * x
 	// }
 }
+
+// Iterative Power (Binary Exponentiation)
+/*
+	3^10 = 3^8 * 3^2
+	10 in binary is 1010. We traverse starting from LSB to MSB and for each bit we traverse we consider it
+	as a corresponding power of x (3). i.e.
+	MSB -----> LSB
+	1     0    1    0
+	\|/  \|/  \|/  \|/
+	3^8  3^4  3^2  3^1
+	we consider value corresponding to 1 and multiply them correspondingly. here we get:
+	answer = 3^8 * 3^2
+	----------------------------------------------------------------------------------------
+	3^19 = 3^16 * 3^2 * 3^1
+	Every number can be written as sum of powers of 2(set bits in binary representation)
+	We can traverse through all bits of a number in O(logn) time
+*/
+func pow3(x, n int) int {
+	res := 1
+	for n > 0 {
+		if n%2 != 0 {
+			res = res * x
+		}
+		n = n / 2
+		x = x * x
+	}
+
+	return res
+}
