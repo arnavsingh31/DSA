3151c834f7a73f39a961a00bd7584cf0ed1df115
Arnav <arnav.xddd@gmail.com>
2023-06-27 12:46:51 +0530

    find prime numbers upto a given integer

---
+++ Random/primeNumbers.go
@@ -0,0 +1,45 @@
+package main
+
+// Sieve of Eratosthenes algorithm
+// T.C ---> O(n*log(logn)) (proof is too complex)
+// we can avoid extra space by directly printing the prime numbers.
+func primeNumbers(n int) []int {
+	arr := []bool{true, true}
+	for i := 2; i <= n+1; i++ {
+		arr = append(arr, true)
+	}
+
+	for i := 2; i*i <= n; i++ {
+		if arr[i] {
+			for j := 2 * i; j <= n; j += i {
+				arr[j] = false
+			}
+		}
+	}
+	ans := []int{}
+	for i := 1; i <= n; i++ {
+		if arr[i] {
+			ans = append(ans, i)
+		}
+	}
+	return ans
+}
+
+// T.C ---> O(n*log(logn)) (proof is too complex)
+// we can avoid extra space by directly printing the prime numbers.
+func primeNumbers2(n int) []int {
+	arr := []bool{true, true}
+	for i := 2; i <= n+1; i++ {
+		arr = append(arr, true)
+	}
+	ans := []int{}
+	for i := 2; i <= n; i++ {
+		if arr[i] {
+			ans = append(ans, i)
+			for j := i * i; j <= n; j += i {
+				arr[j] = false
+			}
+		}
+	}
+	return ans
+}
