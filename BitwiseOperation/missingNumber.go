25fba88c6ea5e39389b08fc1fab7619b98849fdf
Arnav <arnav.xddd@gmail.com>
2023-07-02 16:32:42 +0530

    find missing number

---
+++ BitwiseOperation/missingNumber.go
@@ -0,0 +1,34 @@
+package main
+
+func missingNumber(arr []int) int {
+	n := len(arr)
+	sum := (n * (n + 1)) / 2
+	hasZero := false
+	for i := 0; i < n; i++ {
+		if arr[i] == 0 {
+			hasZero = true
+		}
+		sum -= arr[i]
+
+	}
+	if sum == 0 && hasZero {
+		return n
+	} else if sum == 0 && !hasZero {
+		return 0
+	}
+	return sum
+}
+
+// using bit-operation
+func missingNumber2(arr []int) int {
+	sum := 0
+
+	for i := 0; i <= len(arr); i++ {
+		sum ^= i
+	}
+
+	for _, num := range arr {
+		sum ^= num
+	}
+	return sum
+}
