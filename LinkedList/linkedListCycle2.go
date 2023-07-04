2509df5ccdcd68423e6f2bbad9021ddcab90b426
Arnav <arnav.xddd@gmail.com>
2023-07-05 02:23:15 +0530

    linked list cycle 2

---
+++ LinkedList/linkedListCycle2.go
@@ -0,0 +1,50 @@
+package main
+
+func cycleStart(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	currentNode := head
+	nodeVisitedMap := make(map[*ListNode]bool)
+	for currentNode != nil {
+		if nodeVisitedMap[currentNode] {
+			return currentNode
+		}
+
+		nodeVisitedMap[currentNode] = true
+		currentNode = currentNode.Next
+	}
+
+	return nil
+}
+
+// if cycle is present reset fast to head and keep slow at it current node and now move slow and fast at
+// same pace and where they will meet is the starting node of cycle.
+func cycleStart2(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	fast, slow := head, head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+
+		if fast == slow {
+			break
+		}
+	}
+
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+
+	fast = head
+
+	for fast != slow {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	return fast
+}
