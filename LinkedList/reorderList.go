a417369b866c2cbbf5016c3ac54f81766f53a920
Arnav <arnav.xddd@gmail.com>
2023-07-06 23:57:39 +0530

    indentation

--- LinkedList/reorderList.go
+++ LinkedList/reorderList.go
@@ -30,17 +30,17 @@ func reverseList(start *ListNode) *ListNode {
 }
 
 /*
-First we intend to divide list into 2 halves and then we reverse the second half, after
-reversing we iterate over first and second half while changing the order of the list.
-Using fast and slow algorithm we find the splitting node of the list which is the node pointed by slow
-when fast becomes nil or fast.next is nil.
-After finding splitting node, we reverse the second half of list.
-Now we iterate over 2 halves while moving nodes to modify our original list.
-** Note: List 2 will have less nodes than list 1 that's y we iterate till secondHalf is not nil.
-If node from firstHalf is not equal to slow then we simply move to next node in firstHalf list. When node
-becomes equal to slow we know we have reached end of firstHalf list, so we add nil to our last node in the
-firstHalf list. **OR we can simply point slow.Next = nil as we know it is the last node in the firstHalf.
-T.C---> O(n), S.C---->O(1)
+	First we intend to divide list into 2 halves and then we reverse the second half, after
+	reversing we iterate over first and second half while changing the order of the list.
+	Using fast and slow algorithm we find the splitting node of the list which is the node pointed by slow
+	when fast becomes nil or fast.next is nil.
+	After finding splitting node, we reverse the second half of list.
+	Now we iterate over 2 halves while moving nodes to modify our original list.
+	** Note: List 2 will have less nodes than list 1 that's y we iterate till secondHalf is not nil.
+	If node from firstHalf is not equal to slow then we simply move to next node in firstHalf list. When node
+	becomes equal to slow we know we have reached end of firstHalf list, so we add nil to our last node in the
+	firstHalf list. **OR we can simply point slow.Next = nil as we know it is the last node in the firstHalf.
+	T.C---> O(n), S.C---->O(1)
 */
 func reorderList2(head *ListNode) {
 	slow := head
