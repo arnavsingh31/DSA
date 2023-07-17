48b8909150bdce92ecf342e89f3948c228fab47b
Arnav <arnav.xddd@gmail.com>
2023-07-18 01:39:51 +0530

    convert given absolute path to canonical path

---
+++ Stack/simplifyPath.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"strings"
+)
+
+// LC #71
+func simplifyPath(path string) string {
+	stack := []string{}
+
+	for _, str := range strings.Split(path, "/") {
+		if str != "" && str != "." && str != ".." {
+			stack = append(stack, str)
+		} else if str == ".." && len(stack) > 0 {
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return "/" + strings.Join(stack, "/")
+}
+
+func simplifyPath2(path string) string {
+	stack := []string{}
+	curr := ""
+	for _, char := range path + "/" {
+		if char != '/' {
+			curr += string(char)
+		} else if char == '/' {
+			if curr == ".." {
+				if len(stack) > 0 {
+					stack = stack[:len(stack)-1]
+				}
+			} else if curr != "" && curr != "." {
+				stack = append(stack, curr)
+				curr = ""
+			}
+		}
+	}
+
+	return "/" + strings.Join(stack, "/")
+}
+
+func main() {
+	log.Print(simplifyPath2("/../"))
+}
