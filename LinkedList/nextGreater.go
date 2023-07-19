9799ce97be8fa14375b1d0f6f1ac9f460ac176a6
Arnav <arnav.xddd@gmail.com>
2023-07-20 02:54:53 +0530

    solved using monotonic stack

--- LinkedList/nextGreater.go
+++ LinkedList/nextGreater.go
@@ -26,3 +26,32 @@ func nextGreater(head *ListNode) []int {
 
 	return ans
 }
+
+/*
+	Optimised Solution using monotonic stack
+	T.C--> O(n)
+	S.C--> O(n)
+
+*/
+func nextGreater2(head *ListNode) []int {
+	stack := []int{}
+	arr := make([]int, 0)
+	nextGr := make([]int, 0)
+
+	// convert linked list value to array
+	for currNode := head; currNode != nil; currNode = currNode.Next {
+		arr = append(arr, currNode.Val)
+		nextGr = append(nextGr, 0)
+	}
+
+	for i := 0; i < len(arr); i++ {
+		for len(stack) > 0 && arr[stack[len(stack)-1]] < arr[i] {
+			stackTop := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			nextGr[stackTop] = arr[i]
+		}
+
+		stack = append(stack, i)
+	}
+	return nextGr
+}
