5c39c272c03b1976d23fe3a01b803b8bbde84b38
Arnav <arnav.xddd@gmail.com>
2023-07-31 19:21:16 +0530

    remove nodes from linked list

---
+++ Stack/removeNodesFromLL.go
@@ -0,0 +1,33 @@
+package main
+
+/*
+	LC #2487
+	TC---> O(n)
+	SC---> O(n)
+	using monotonic stack
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNodes(head *ListNode) *ListNode {
+	dec_stack := make([]*ListNode, 0)
+	currNode := head
+
+	for currNode != nil {
+		for len(dec_stack) > 0 && dec_stack[len(dec_stack)-1].Val < currNode.Val {
+			dec_stack = dec_stack[:len(dec_stack)-1]
+		}
+		dec_stack = append(dec_stack, currNode)
+		currNode = currNode.Next
+	}
+
+	head = dec_stack[0]
+	for i := 0; i < len(dec_stack)-1; i++ {
+		dec_stack[i].Next = dec_stack[i+1]
+	}
+	dec_stack[len(dec_stack)-1].Next = nil
+	return head
+}
