503693504ce6bede4e3d52a2b359948f0eabda9a
Arnav <arnav.xddd@gmail.com>
2023-08-03 22:53:19 +0530

    majority element 2 optimised

--- HashMaps/majorityElement2.go
+++ HashMaps/majorityElement2.go
@@ -23,4 +23,54 @@ func majorityElement(nums []int) []int {
 	return ans
 }
 
-// Todo: Implement using boyer-moore voting algorithm
+/*
+	TC--->O(n)
+	SC--->O(1)
+	Using Boyer-Moore Majority voting algorithm
+*/
+func majorityElement2(nums []int) []int {
+	ans := []int{}
+	n := len(nums)
+	candidate1 := -1
+	candidate2 := -1
+	count1 := 0
+	count2 := 0
+
+	for _, num := range nums {
+		if num == candidate1 {
+			count1++
+		} else if num == candidate2 {
+			count2++
+		}
+
+		if count1 == 0 {
+			candidate1 = num
+			count1++
+		} else if count2 == 0 {
+			candidate2 = num
+			count2++
+		} else {
+			count1--
+			count2--
+		}
+	}
+
+	// now check the candidature of both canditates
+	count1, count2 = 0, 0
+	for _, num := range nums {
+		if num == candidate1 {
+			count1++
+		} else if num == candidate2 {
+			count2++
+		}
+	}
+
+	if count1 > n/3 {
+		ans = append(ans, candidate1)
+	}
+	if count2 > n/3 {
+		ans = append(ans, candidate2)
+	}
+
+	return ans
+}
