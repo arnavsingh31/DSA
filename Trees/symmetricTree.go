e70fae12b040f31e360506cd685882063902831f
Arnav <arnav.xddd@gmail.com>
2023-08-06 23:27:04 +0530

    is given binary tree symmetric

---
+++ Trees/symmetricTree.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+	LC #101
+	TC---> O(n)
+	SC---> O(n)
+*/
+func isSymmetric(root *Node) bool {
+	return symHelper(root.Left, root.Right)
+}
+
+func symHelper(r1, r2 *Node) bool {
+	if r1 == nil && r2 == nil {
+		return true
+	}
+
+	if r1 == nil || r2 == nil || r1.Val != r2.Val {
+		return false
+	}
+
+	return symHelper(r1.Left, r2.Right) && symHelper(r1.Right, r2.Left)
+
+}
