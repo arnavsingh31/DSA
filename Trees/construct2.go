3b12a2034f25c1e501ac7f956b9932e711684b22
Arnav <arnav.xddd@gmail.com>
2023-08-07 23:35:20 +0530

    build tree from inorder and postorder traversal

---
+++ Trees/construct2.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+	LC #106
+	TC---> O(n)
+	SC---> O(n)
+*/
+func buildTree2(inorder, postorder []int) *Node {
+	if len(inorder) == 0 {
+		return nil
+	}
+
+	root := &Node{Val: postorder[len(postorder)-1]}
+	i := 0
+	for ; i < len(inorder); i++ {
+		if inorder[i] == root.Val {
+			break
+		}
+	}
+
+	root.Left = buildTree2(inorder[:i], postorder[:i])
+	root.Right = buildTree2(inorder[i+1:], postorder[i:len(postorder)-1])
+
+	return root
+}
+
+/*
+	inorder = [9,3,15,20,7]
+	postorder = [9,15,7,20,3]
+
+	root -> 3
+	i- > 1
+	in[:1] = [9]
+	post[:1] = [9]
+
+	root-> 9
+	i-> 0
+	in[:0] = []
+	post[:0] = []
+
+*/
