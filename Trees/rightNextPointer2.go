79e9bb809f2f902108ce1e606cda9e502855f2e8
Arnav <arnav.xddd@gmail.com>
2023-08-09 00:05:08 +0530

    populate right next pointer 2

---
+++ Trees/rightNextPointer2.go
@@ -0,0 +1,43 @@
+package main
+
+/*
+	LC #117
+	TC---> O(n)
+	SC---> O(k)
+*/
+func connect2(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	queue := []*TreeNode{root}
+
+	appendToQueue := func(node *TreeNode) {
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+	}
+
+	for len(queue) > 0 {
+		prev := queue[0]
+		queue = queue[1:]
+
+		levelLength := len(queue)
+		appendToQueue(prev)
+
+		for i := 0; i < levelLength; i++ {
+			pop := queue[0]
+			queue = queue[1:]
+			appendToQueue(pop)
+
+			prev.Next = pop
+			prev = pop
+		}
+
+	}
+	return root
+}
