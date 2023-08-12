68925033a2d1b1731177dc1598311bdf106ea772
Arnav <arnav.xddd@gmail.com>
2023-08-13 00:47:31 +0530

    find all root to leaf paths for a binary tree

---
+++ Trees/treePaths.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+LC #257
+TC--->O(n)
+SC--->O(n)
+*/
+func binaryTreePaths(root *Node) []string {
+	res := []string{}
+
+	pathHelper(root, "", &res)
+	return res
+}
+
+func pathHelper(root *Node, path string, res *[]string) {
+	if root == nil {
+		return
+	}
+
+	seperator := ""
+
+	if path != "" {
+		seperator = "->"
+	}
+
+	path += fmt.Sprintf("%s%d", seperator, root.Val)
+
+	if root.Left == nil && root.Right == nil {
+		*res = append(*res, path)
+		return
+	}
+
+	pathHelper(root.Left, path, res)
+	pathHelper(root.Right, path, res)
+
+}
