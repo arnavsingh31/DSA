c92e147882b3f486c4a6b6966ebf4768a30a39f5
Arnav <arnav.xddd@gmail.com>
2023-08-16 23:56:53 +0530

    jump game 1

---
+++ Arrays/jumpGame1.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+	LC #55
+	TC--->O(n)
+	SC--->O(1)
+*/
+func canJump(arr []int) bool {
+	can_reach := arr[0]
+
+	for i := 0; i <= can_reach; i++ {
+		if i == len(arr)-1 {
+			return true
+		}
+		if can_reach < i+arr[i] {
+			can_reach = i + arr[i]
+		}
+
+		if can_reach >= len(arr)-1 {
+			return true
+		}
+	}
+	return false
+}
