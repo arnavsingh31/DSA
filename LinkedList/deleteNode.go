02229ce04061ee359e078c98420aba18a990367a
Arnav <arnav.xddd@gmail.com>
2023-09-04 14:34:09 +0530

    optimised

--- LinkedList/deleteNode.go
+++ LinkedList/deleteNode.go
@@ -4,25 +4,58 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	if head == nil {
 		return head
 	}
-
 	length := lengthOfList(head)
-	nodeBeforeNthNode := length - n
 
+	pos := length - n
 	dummy := &ListNode{Next: head}
 	prev := dummy
 
 	currNode := prev.Next
 
-	for nodeBeforeNthNode > 1 {
+	for currNode != nil {
+		if pos == 0 {
+			break
+		}
+
+		pos--
+		prev = currNode
 		currNode = currNode.Next
-		nodeBeforeNthNode--
 	}
 
-	if length == n {
-		prev.Next = currNode.Next
-	} else {
-		currNode.Next = currNode.Next.Next
+	prev.Next = currNode.Next
+	currNode.Next = nil
+
+	return dummy.Next
+}
+
+/*
+Two-Pointer approach.
+*/
+func removeNthFromEnd2(head *ListNode, n int) *ListNode {
+	if head == nil {
+		return head
 	}
+
+	dummy := &ListNode{Next: head}
+	prev := dummy
+	node1, node2 := prev.Next, prev.Next
+
+	for n > 0 {
+		n--
+		node2 = node2.Next
+	}
+
+	// if node2 is nil then n = length of linked list
+	if node2 == nil {
+		return head.Next
+	}
+
+	for node2.Next != nil {
+		node1 = node1.Next
+		node2 = node2.Next
+	}
+
+	node1.Next = node1.Next.Next
 
 	return dummy.Next
 }
