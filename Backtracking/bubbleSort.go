971d148d39e2c378dc52a44645f2367aacd86b0f
Arnav <arnav.xddd@gmail.com>
2023-10-20 21:57:52 +0530

    bubble sort

---
+++ Backtracking/bubbleSort.go
@@ -0,0 +1,16 @@
+package backtracking
+
+/*
+	TC--->O(n*n)
+	SC--->O(1)
+*/
+func BubbleSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
