8c36e6b89af9efe3a7d11e61ea78a1d70aac0369
Arnav Singh <arnav.xddd@gmail.com>
2024-09-16 10:02:49 +0530

    plus one problem

--- revision.go
+++ revision.go
@@ -1803,3 +1803,19 @@ func completeCircuit(cost, gas []int) int {
 
 	return -1
 }
+
+func addOne(digits []int) []int {
+	carry := 1
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		sum := digits[i] + carry
+		digits[i] = sum % 10
+		carry = sum / 10
+	}
+
+	if carry > 0 {
+		digits = append([]int{carry}, digits...)
+	}
+
+	return digits
+}
